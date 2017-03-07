5ee000253eb0997945eafc106e28deb88884c7e5
kshatra <ajith.singh.kshatri@adp.com>
2017-03-06 20:13:28 -0500

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Kshatri, Ajith Singh (CORP) on 3/6/17.
+//  Copyright © 2017 Kshatri, Ajith Singh (CORP). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
